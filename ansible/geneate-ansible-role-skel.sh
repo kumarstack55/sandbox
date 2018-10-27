39cd7a77c517e88f359476436a139e734ff10f55
kumarstack55 <kumarstack55@gmail.com>
2018-10-27 14:38:40 +0900

    fix

--- ansible/geneate-ansible-role-skel.sh
+++ ansible/geneate-ansible-role-skel.sh
@@ -3,12 +3,46 @@
 set -eu
 
 gloval_variables() {
-  TEMPLATE_DEST_LIST=template_dest_list.txt
-  UNIT_LIST=unit_list.txt
+  FNAME_CPIO_IT=cpio_it.txt
+  FNAME_TEMPLATE_DEST_LIST=template_dest_list.txt
+  FNAME_UNIT_LIST=unit_list.txt
+  MSG_SKIP_DRYRUN="skip. (dry-run)"
+  MSG_SKIP_FILE_EXISTS="skip. (file already exists)"
 
-  g_mail=kumarstack55@gmail.com
-  g_user=kumarstack55
-  option_dry_run=yes
+  option_dry_run=no
+  option_force_overwrite=no
+  option_leave_tmp_dir=no
+  option_license="GPLv3"
+
+  env_author_info=""
+  env_github_username=""
+}
+
+read_vars_from_env() {
+  local rc_dir="$HOME/.config/generate-ansible-role-skel"
+  mkdir -pv $rc_dir
+
+  local rc_file="$rc_dir/config.sh"
+  if [[ ! -f $rc_file ]]; then
+    echo "# Generating: $rc_file"
+    cat <<__BASH__ | tee $rc_file >/dev/null
+#AUTHOR_INFO="xxx"
+#GITHUB_USERNAME="xxx"
+__BASH__
+  fi
+  source $rc_file
+
+  # TODO: hard coding
+  if [[ ! -v AUTHOR_INFO ]]; then
+    die "Please define AUTHOR_INFO in $rc_file"
+  fi
+  env_author_info="$AUTHOR_INFO"
+
+  # TODO: hard coding
+  if [[ ! -v GITHUB_USERNAME ]]; then
+    die "Please define GITHUB_USERNAME in $rc_file"
+  fi
+  env_github_username="$GITHUB_USERNAME"
 }
 
 usage_exit() {
@@ -16,8 +50,10 @@ usage_exit() {
   echo_err "usage: $prog [OPTIONS...] RPM_PKG_NAME"
   echo_err ""
   echo_err "options:"
-  echo_err " --dry-run           : Don't make any change (default)"
-  echo_err " -f, --force         : Force mode"
+  echo_err " --dry-run       : Don't make any change"
+  echo_err " -f, --force     : Force overwrite"
+  echo_err " --dest-dir      : Destination directory"
+  echo_err " --leave-tmp-dir : Don't remove temporary directory"
   exit 1
 }
 
@@ -48,10 +84,28 @@ die() {
 }
 
 generate_readme() {
+  local tmp_dir="$1"; shift
   local role_name="$1"; shift
+  local init="$1"; shift
   local rpm_pkg_name="$1"; shift
 
   local ofile=$role_name/README.md
+  echo "# Generating: $ofile."
+  if [[ $option_dry_run == yes ]]; then
+    echo $MSG_SKIP_DRYRUN
+    echo
+    return
+  fi
+
+  if [[
+    $init != yes && -f $ofile &&
+    $option_force_overwrite != yes
+  ]]; then
+    echo $MSG_SKIP_FILE_EXISTS
+    echo
+    return
+  fi
+
   : >$ofile
 
   cat <<__README_MD__ | tee -a $ofile >/dev/null
@@ -81,31 +135,51 @@ Example Playbook
 \`\`\`yaml
 - hosts: servers
   roles:
-    - role: ${g_user}.${rpm_pkg_name}
+    - role: ${env_github_username}.${rpm_pkg_name}
 \`\`\`
 
 License
 -------
 
-GPLv3
+$option_license
 
 Author Information
 ------------------
 
-$g_mail
+$env_author_info
 __README_MD__
+
+  echo
 }
 
 generate_meta() {
+  local tmp_dir="$1"; shift
   local role_name="$1"; shift
+  local init="$1"; shift
   local rpm_pkg_name="$1"; shift
 
   local ofile=$role_name/meta/main.yml
+  echo "# Generating: $ofile."
+  if [[ $option_dry_run == yes ]]; then
+    echo $MSG_SKIP_DRYRUN
+    echo
+    return
+  fi
+
+  if [[
+    $init != yes && -f $ofile &&
+    $option_force_overwrite != yes
+  ]]; then
+    echo $MSG_SKIP_FILE_EXISTS
+    echo
+    return
+  fi
+
   : >$ofile
 
   cat <<__YAML__ | tee -a $ofile >/dev/null
 galaxy_info:
-  author: $g_user
+  author: $env_github_username
   description: $rpm_pkg_name
   license: GPL
   min_ansible_version: 1.2
@@ -116,13 +190,33 @@ galaxy_info:
   galaxy_tags: [ $rpm_pkg_name ]
 dependencies: []
 __YAML__
+
+  echo
 }
 
 generate_tasks() {
+  local tmp_dir="$1"; shift
   local role_name="$1"; shift
+  local init="$1"; shift
   local rpm_pkg_name="$1"; shift
 
   local ofile=$role_name/tasks/main.yml
+  echo "# Generating: $ofile."
+  if [[ $option_dry_run == yes ]]; then
+    echo $MSG_SKIP_DRYRUN
+    echo
+    return
+  fi
+
+  if [[
+    $init != yes && -f $ofile &&
+    $option_force_overwrite != yes
+  ]]; then
+    echo $MSG_SKIP_FILE_EXISTS
+    echo
+    return
+  fi
+
   : >$ofile
 
   cat <<__YAML__ | tee -a $ofile >/dev/null
@@ -147,7 +241,7 @@ generate_tasks() {
   loop:
 __YAML__
 
-  cat ./output/$TEMPLATE_DEST_LIST \
+  cat $tmp_dir/output/$FNAME_TEMPLATE_DEST_LIST \
   | while read -r dest; do
       cat <<__YAML__ | tee -a $ofile >/dev/null
     - dest: $dest
@@ -158,7 +252,7 @@ __YAML__
 #  notify:
 __YAML__
 
-  cat ./output/$UNIT_LIST \
+  cat $tmp_dir/output/$FNAME_UNIT_LIST \
   | while read -r unit; do
       cat <<__YAML__ | tee -a $ofile >/dev/null
 #    - Ensure ${unit} service restarted
@@ -174,7 +268,7 @@ __YAML__
 #  loop:
 __YAML__
 
-  cat ./output/$UNIT_LIST \
+  cat $tmp_dir/output/$FNAME_UNIT_LIST \
   | while read -r unit; do
       cat <<__YAML__ | tee -a $ofile >/dev/null
 #    - ${unit}.service
@@ -185,39 +279,75 @@ __YAML__
 #  notify:
 __YAML__
 
-  cat ./output/$UNIT_LIST \
+  cat $tmp_dir/output/$FNAME_UNIT_LIST \
   | while read -r unit; do
       cat <<__YAML__ | tee -a $ofile >/dev/null
 #    - Ensure ${unit} service restarted
 __YAML__
     done
+
+  echo
 }
 
 generate_templates() {
+  local tmp_dir="$1"; shift
   local role_name="$1"; shift
+  local init="$1"; shift
   local rpm_pkg_name="$1"; shift
 
-  cat ./output/$TEMPLATE_DEST_LIST \
+  cat $tmp_dir/output/$FNAME_TEMPLATE_DEST_LIST \
   | while read -r line; do
       local tmpl_dest="$line"
       local tmpl_src="$role_name/templates/el7${line}.j2"
       local tmpl_src_dir=$(dirname $tmpl_src)
 
+      echo "# Generating: $tmpl_src"
+      if [[ $option_dry_run == yes ]]; then
+        echo $MSG_SKIP_DRYRUN
+        echo
+        return
+      fi
+      if [[ -f $tmpl_src && $option_force_overwrite != yes ]]; then
+        echo $MSG_SKIP_FILE_EXISTS
+        echo
+        return
+      fi
+
       mkdir -pv $tmpl_src_dir
-      rpm2cpio $pkg_file \
+      rpm2cpio $pkg_path \
         | cpio -i --to-stdout .$line \
         | tee $tmpl_src >/dev/null
+
+      echo
     done
 }
 
 generate_handlers() {
+  local tmp_dir="$1"; shift
   local role_name="$1"; shift
+  local init="$1"; shift
   local rpm_pkg_name="$1"; shift
 
   local ofile=$role_name/handlers/main.yml
+  echo "# Generating: $ofile."
+  if [[ $option_dry_run == yes ]]; then
+    echo $MSG_SKIP_DRYRUN
+    echo
+    return
+  fi
+
+  if [[
+    $init != yes && -f $ofile &&
+    $option_force_overwrite != yes
+  ]]; then
+    echo $MSG_SKIP_FILE_EXISTS
+    echo
+    return
+  fi
+
   : >$ofile
 
-  cat ./output/$UNIT_LIST \
+  cat $tmp_dir/output/$FNAME_UNIT_LIST \
     | while read -r unit; do
         cat <<__YAML__ | tee -a $ofile >/dev/null
 - name: Ensure ${unit} service restarted
@@ -227,61 +357,112 @@ generate_handlers() {
 
 __YAML__
       done
+
+  echo
+}
+
+generate_tests() {
+  local tmp_dir="$1"; shift
+  local role_name="$1"; shift
+  local init="$1"; shift
+  local rpm_pkg_name="$1"; shift
+
+  local ofile=$role_name/tests/test.yml
+  echo "# Generating: $ofile."
+  if [[ $option_dry_run == yes ]]; then
+    echo $MSG_SKIP_DRYRUN
+    echo
+    return
+  fi
+
+  if [[
+    $init != yes && -f $ofile &&
+    $option_force_overwrite != yes
+  ]]; then
+    echo $MSG_SKIP_FILE_EXISTS
+    echo
+    return
+  fi
+
+  : >$ofile
+
+  cat <<__YAML__ | tee -a $ofile >/dev/null
+---
+- hosts: localhost
+  gather_facts: no
+  become: yes
+  roles:
+    - ansible-role-mlocate
+__YAML__
+
+  echo
 }
 
 main() {
   local rpm_pkg_name="$1"; shift
 
-  tmp_dir=$(mktemp -d)
-  pushd $tmp_dir >/dev/null
+  local tmp_dir=$(mktemp -d)
+  local rpm_dir="$tmp_dir/rpm"
+  local out_dir="$tmp_dir/output"
 
-  mkdir -p ./rpm
-  yumdownloader --destdir ./rpm $rpm_pkg_name 2>&1 >/dev/null
-  pkg_file=$(readlink -f $(find ./rpm -type f))
+  echo "# Downloading rpm package..."
+  mkdir -p $rpm_dir
+  yumdownloader --destdir $rpm_dir $rpm_pkg_name 2>&1 >/dev/null
+  local pkg_path=$(readlink -f $(find $rpm_dir -type f))
+  echo "rpm file: $pkg_path"
 
-  mkdir -pv ./output
-
-  rpm2cpio $pkg_file \
+  echo "# Listing files from package..."
+  mkdir -pv $out_dir
+  rpm2cpio $pkg_path \
     | cpio -it \
     | sed -e 's/^\.//' \
-    >./output/cpio_it.txt
+    >$out_dir/$FNAME_CPIO_IT
 
+  echo "# Listing destination path..."
   cat \
-    <(grep -P "^(/etc/|/var/lib/)" ./output/cpio_it.txt) \
-    <(rpm -q -p $pkg_file -l | grep -P "^(/etc/|/var/lib/)") \
+    <(grep -P "^(/etc/|/var/lib/)" $out_dir/$FNAME_CPIO_IT) \
+    <(rpm -q -p $pkg_path -l | grep -P "^(/etc/|/var/lib/)") \
     | sort \
     | uniq --repeated \
-    >./output/$TEMPLATE_DEST_LIST
+    >$out_dir/$FNAME_TEMPLATE_DEST_LIST
 
-  rpm -q -p $pkg_file -l \
+  echo "# Listing systemd units..."
+  rpm -q -p $pkg_path -l \
     | grep -Po "(?<=^/usr/lib/systemd/system/).+(?=.service)" \
-    >./output/$UNIT_LIST
+    >$out_dir/$FNAME_UNIT_LIST
 
+  echo "# Initializing ansible role..."
   local role_name="ansible-role-${rpm_pkg_name}"
-
-  ansible-galaxy init $role_name
-  generate_readme $role_name $rpm_pkg_name
-  generate_meta $role_name $rpm_pkg_name
-  generate_tasks $role_name $rpm_pkg_name
-  generate_templates $role_name $rpm_pkg_name
-  generate_handlers $role_name $rpm_pkg_name
-  #generate_tests $role_name $rpm_pkg_name # TODO
-
+  local init=no
   if [[ $option_dry_run == 'yes' ]]; then
-    find .
-    echo_err "(dry run)"
+    echo $MSG_SKIP_DRYRUN
+  elif [[
+      ( -e $role_name && $option_force_overwrite == yes ) ||
+      ( ! -e $role_name )
+    ]]; then
+    init=yes
+    ansible-galaxy init $role_name
   fi
-  popd >/dev/null
 
-  if [[ $option_dry_run == 'no' ]]; then
-    mv -iv $tmp_dir/$role_name ./$role_name
+  echo "# Generating directory and files..."
+  generate_readme    $tmp_dir $role_name $init $rpm_pkg_name
+  generate_meta      $tmp_dir $role_name $init $rpm_pkg_name
+  generate_tasks     $tmp_dir $role_name $init $rpm_pkg_name
+  generate_templates $tmp_dir $role_name $init $rpm_pkg_name
+  generate_handlers  $tmp_dir $role_name $init $rpm_pkg_name
+  generate_tests     $tmp_dir $role_name $init $rpm_pkg_name
+
+  if [[ $option_leave_tmp_dir == 'yes' ]]; then
+    echo "# To remove temporary directory, type:"
+    echo "$ rm -rf $tmp_dir"
+  else
+    echo "# Removing temporary directory."
+    rm -rf $tmp_dir
   fi
-
-  echo_err "removing temporary files..."
-  rm -rf $tmp_dir
 }
 
 gloval_variables
+read_vars_from_env
 
 while getopts -- "-:fh" OPT; do
   case $OPT in
@@ -291,14 +472,17 @@ while getopts -- "-:fh" OPT; do
           option_dry_run=yes
           ;;
         force)
-          option_dry_run=no
+          option_force_overwrite=yes
+          ;;
+        leave-tmp-directory)
+          option_leave_tmp_dir=yes
           ;;
         *)
           getopt_err_badopt $OPTARG
           usage_exit
           ;;
       esac;;
-    f) option_dry_run=no;;
+    f) option_force_overwrite=yes;;
     h) usage_exit;;
     \?) usage_exit;;
   esac
@@ -306,6 +490,7 @@ done
 shift $((OPTIND-1))
 
 if [[ $# -ne 1 ]]; then
+  die "no rpm package name"
   usage_exit
 fi
 
